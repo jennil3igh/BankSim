d2994a83af9d01390d87b11dc1b45d6b070760be
alex <alex@alex>
2018-10-23 20:58:21 -0400

    Working Project; all Tasks accomplished. Comments included to help us demo

--- src/edu/temple/cis/c3238/banksim/Account.java
+++ src/edu/temple/cis/c3238/banksim/Account.java
@@ -1,5 +1,8 @@
 package edu.temple.cis.c3238.banksim;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author Cay Horstmann
  * @author Modified by Paul Wolfgang
@@ -10,17 +13,25 @@ public class Account {
     private volatile int balance;
     private final int id;
     private final Bank myBank;
-
+    
     public Account(Bank myBank, int id, int initialBalance) {
         this.myBank = myBank;
         this.id = id;
         balance = initialBalance;
     }
 
-    public synchronized int getBalance() {
+    /* 
+     * getBalance() does not need to be synchronized, despite balance being
+     * a shared variable, because getBalance() does not modify balance. 
+     **/
+    public int getBalance() {
         return balance;
     }
 
+    /* 
+     * withdraw() method is synchronized via synchronized keyword in it's
+     * declaration 
+     **/
     public synchronized boolean withdraw(int amount) {
         if (amount <= balance) {
             int currentBalance = balance;
@@ -33,13 +44,29 @@ public class Account {
         }
     }
 
+    /* 
+     * synchronized same as withdraw()
+     **/
     public synchronized void deposit(int amount) {
         int currentBalance = balance;
         //Thread.yield();   // Try to force collision
         int newBalance = currentBalance + amount;
         balance = newBalance;
         
-        //notifyAll();
+        notifyAll();
+    }
+    
+    /*
+     * implementation of task 4
+     * this method waits for amount <= balance 
+     **/
+    public synchronized void waitForSufficientFunds(int amount) {
+        
+        while (myBank.isOpen() && amount >= balance) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {/*ignore*/}
+        }
     }
     
     @Override
